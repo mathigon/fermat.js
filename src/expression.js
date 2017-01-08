b92a72253c4d4b7a043872b0ae6158104f4b175e
Philipp Legner <philipp@mathigon.org>
2017-01-08 23:30:24 +0000

    Better expression simplification, many more tests, bug fixes

--- src/expression.js
+++ src/expression.js
@@ -5,7 +5,19 @@
 
 
 
-import { factorial, permutations } from 'combinatorics'
+import { isNumber, isString } from 'types';
+import { factorial, permutations } from 'combinatorics';
+
+
+// -----------------------------------------------------------------------------
+// Error handling
+
+class ExprError extends Error {
+  constructor(name, message) {
+    super(message);
+    this.name = name;
+  }
+}
 
 
 // -----------------------------------------------------------------------------
@@ -33,109 +45,178 @@ function equals(expr1, expr2) {
 
 // -----------------------------------------------------------------------------
 // Expression Simplification
-// TODO more and better simplification rules
-// TODO exact decimals / don't simplify
+// TODO improved simplification rules
+// TODO exact decimals
 
-const FN_SIMPLIFY = {
-  '+': function(...args) {
-    let newArgs = [];
-    for (let a of args) {
-      newArgs.push(...(a[0] == '+' ? a.slice(1) : [a]))
-    }
+let EXPAND_RULES = [
+  // Ordering
+  ['a-(b-c)', '(a+c)-b'],
+  ['(a-b)-c', 'a-(b+c)'],
+  ['a/(b/c)', '(a*c)/b'],
+  ['(a/b)/c', 'a/(b*c)'],
+  ['-(-a)', 'a'],
 
-    let numbers = newArgs.filter(a => typeof a == 'number');
-    let others = newArgs.filter(a => typeof a != 'number');
+  // Distributive laws
+  ['(a+b)*c', 'a*c+b*c'],
+  ['(a-b)*c', 'a*c-b*c'],
+  ['(a+b)/c', 'a/c+b/c'],
+  ['(a-b)/c', 'a/c-b/c'],
+  ['a*(b+c)', 'a*b+a*c'],
+  ['a*(b-c)', 'a*b-a*c'],
 
-    let constant = FN_EVALUATE['+'](...numbers);
-    if (!others.length) return constant;
+  // Expansions
+  ['(a+b)^2', 'a^2+2*a*b+b^2'],
+  ['(a-b)^2', 'a^2-2*a*b+b^2'],
+  ['(a+b)*(c+d)', 'a*c+a*d+b*c+b*d'],
+  ['(a+b)*(c-d)', 'a*c-a*d+b*c-b*d'],
+  ['(a-b)*(c+d)', 'a*c+a*d-b*c-b*d'],
+  ['(a-b)*(c-d)', 'a*c-a*d-b*c+b*d'],
 
-    // TODO remove constant 0 if others.length
-    // TODO _a + _a = 2*a
-    // TODO _1+_2*_1 == (1+_2)*_1
-    // TODO nice ordering
-    // TODO (_1*_2)+(_3*_2) == (_1+_3)*_2
+  // Power laws
+  ['(a*b)^c', 'a^c*b^c'],
+  ['(a/b)^c', 'a^c/b^c'],
+  ['a^(b+c)', 'a^b*a^c'],
+  ['a^(b-c)', 'a^b/a^c'],
+  ['(a^b)^c', 'a^(b*c)'],
 
-    if (constant) return ['+', constant, ...others];
-    return ['+', constant, ...others];
-  },
-  '-': function(a, b) {
-    // TODO remove 0s
-    // TODO _a - _a = 0
-  },
-  '*': function(...args) {
-    // Remove zeros.
-    for (let a of args) if (a === 0) return 0;
+  // Log laws
+  ['log(a^b)', 'b*log(a)'],
+  ['log(a*b)', 'log(a)+log(b)'],
+  ['log(a-b)', 'log(a)-log(b)'],
 
-    let newArgs = [];
-    for (let a of args) {
-      newArgs.push(...(a[0] == '*' ? a.slice(1) : [a]))
-    }
+  // Power cleanup
+  ['a^(-b)', '1/(a^b)'],
+  ['sqrt(a)', 'a^0.5']
+];
+let HAS_GENERATED_RULES = false;
 
-    let numbers = newArgs.filter(a => typeof a == 'number');
-    let others = newArgs.filter(a => typeof a != 'number');
+function match(expr, target) {
+  // The expression is immediately matched to a target placeholder
+  if (isString(target)) return {[target]: expr};
+  if (isNumber(target) && expr == target) return {};
 
-    let constant = FN_EVALUATE['*'](...numbers);
-    if (!others.length) return constant;
+  if (expr[0] != target[0]) return;
+  if (expr.length != target.length) return;
 
-    // TODO remove constant 1 of others.length
-    // TODO _a * _a = a^2
-    // TODO nice ordering
-    // TODO _a^_b * _a^_c == _a^(_b + _c)   (+ sqrt)
-    // TODO _a^_b * _c^_b == (_a*_b)^_c   (+ sqrt)
-    // TODO a * (b/c) * (d/e) == (a*b*d)/(c*e)
-    // TODO (x + y)*(x - y) == x^2 - y^2
-
-    if (constant != 1) return ['*', constant, ...others];
-    return ['*', ...others];
-  },
-  '/': function(a, b) {
-    // TODO _a / _a = 1
-    // TODO (_1*_2)/_1 == _1
-    if (b === 1) return a
-  },
-  '^': function(a, b) {
-    if (b === 0) return 1;
-    if (a === 0) return 0;
-    if (a === 1) return 1;
-    if (b === 1) return a;
-
-    // (a^b)^c == a^(b*c)
-    if (a[0] == '^') {
-      return ['^', a[1], simplify(['*', a[2], b])]
-    }
-
-    // TODO Bionomial expansions
-
-    if (b === -1) return ['/', 1, a];
-    if (b === 1/2) return ['sqrt', a];
-  },
-  'log': function(a, b) {
-    // TODO ln(_1*_2) == ln(_1)+ln(_2)
-    // TODO ln(_1/_2) == ln(_1)-ln(_2)
+  let placeholders = {};
+  for (let i=1; i<target.length; ++i) {
+    let newPlaceholders = match(expr[i], target[i]);
+    if (!newPlaceholders) return;
+    Object.assign(placeholders, newPlaceholders);
   }
-};
+
+  return placeholders;
+}
+
+function fill(expr, placeholders) {
+  if (isNumber(expr)) return expr;
+  if (isString(expr)) return placeholders[expr];
+  let args = expr.slice(1).map(a => fill(a, placeholders));
+  return [expr[0], ...args];
+}
+
+// TODO simplify the rewrite function, merge with while loop, better efficiency?
+function rewrite(expr, rule) {
+  if (isString(expr) || isNumber(expr)) return;
+
+  let args = expr.slice(1).map(a => rewrite(a, rule));
+  let argsWereRewritten = args.some(a => !!a);
+
+  expr = expr.map((a, i) => args[i-1] || a);
+  let placeholders = match(expr, rule[0]);
+  if (placeholders) return fill(rule[1], placeholders);
+
+  // This expression wasn't rewritten, but one of its children was.
+  if (argsWereRewritten) return expr;
+}
+
+// TODO Improved flattening, remove traverse fn, add ordering functions above
+function flatten([fn, ...args], op1, op2) {
+  if (fn != op1 && fn != op2) return;
+
+  let positive = [];
+  let negative = [];
+  let swap = (args.length < 2);
+
+  for (let a of args) {
+    if (a[0] == op1) {
+      (swap ? negative : positive).push(...a.slice(1))
+    } else if (a[0] == op2) {
+      if (a.length == 3) {  // Only needed for -.
+        (swap ? negative : positive).push(a[1]);
+        (swap ? positive : negative).push(a[2])
+      } else {
+        (swap ? positive : negative).push(a[1])
+      }
+    } else {
+      (swap ? negative : positive).push(a);
+    }
+
+    // For negative operations (- and /), the values of the second argument are inverted.
+    if (fn == op2) swap = 2;
+  }
+
+  let unity = (op2 == '/') ? [1] : [];
+  if (!negative.length) return [op1, ...positive];
+  if (!positive.length) return ['-', ...unity, ['+', ...negative]];
+  return [op2, [op1, ...positive], [op1, ...negative]];
+}
+
+function traverse(expr, callback, ...options) {
+  if (typeof expr == 'number' || typeof expr == 'string') return expr;
+  let [fn, ...args] = expr;
+  args = args.map(a => traverse(a, callback, ...options));
+  return callback([fn, ...args], ...options) || [fn, ...args];
+}
 
 function simplify(expr) {
-  if (typeof expr === 'number' || typeof expr === 'string') {
-    return expr;
-
-  } else {
-    let [fn, ...args] = expr;
-    args = args.map(a => simplify(a));
-
-    // If possible, evaluate this function.
-    if (args.every(a => typeof a == 'number' || a in CONSTANTS)) {
-      return evaluate([fn, ...args]);
-    }
-
-    if (fn in FN_SIMPLIFY) return FN_SIMPLIFY[fn](...args) || [fn, ...args];
-    return [fn, ...args];
+  if (!HAS_GENERATED_RULES) {
+    EXPAND_RULES = EXPAND_RULES.map(rule => [parseString(rule[0]), parseString(rule[1])]).reverse();
+    HAS_GENERATED_RULES = true;
   }
+
+  let changes;
+  do {
+    changes = 0;
+    for (let r of EXPAND_RULES) {
+      let newExpr = rewrite(expr, r);
+      if (newExpr) {
+        changes += 1;
+        expr = newExpr;
+      }
+    }
+  } while(changes);
+
+  expr = traverse(expr, flatten, '*', '/');
+  expr = traverse(expr, flatten, '+', '-');
+
+  // TODO collect and cancel terms (fix 2*a^2-a)
+
+  expr = evaluate(expr, null);
+
+  // TODO clean *0, *1, +0, /1, -0
+
+  return expr;
 }
+
+/* function addFractions([fn, ...args]) {
+  if (fn == '+' || fn == '-') {
+    let fractions = args.filter(a => a[0] == '/');
+    if (fractions.length) {
+      let num = args.map(a => {
+        let den = fractions.filter(f => f != a).map(f => f[1]);
+        return den.length ? ['*', a, ...den] : a;
+      });
+      let den = fractions.map(f => f[1]);
+      return ['/', [fn, ...num], den.length > 1 ? ['*', ...den] : den[0]];
+    }
+  }
+} */
 
 
 // -----------------------------------------------------------------------------
 // Expression Stringify
+// TODO correct bracket placement
 
 const FN_STRINGIFY = {
   '+': (...args) => args.join(' + '),
@@ -145,18 +226,22 @@ const FN_STRINGIFY = {
   '/': (a, b) => a + '/' + b,
   '!': x => x + '!',
   '%': x => x + '%',
-  '^': (a, b) => a + '^' + b
+  'abs': x => '|' + x + '|',
+  '^': (a, b) => a + '^' + b,
+  '=': (a, b) => a + ' = ' + b,
+  '<': (a, b) => a + ' < ' + b,
+  '>': (a, b) => a + ' > ' + b,
+  '≤': (a, b) => a + ' ≤ ' + b,
+  '≥': (a, b) => a + ' ≥ ' + b
 };
 
 function stringify(expr) {
-  if (typeof expr === 'number' || typeof expr === 'string') {
-    return expr;
-  } else {
-    let [fn, ...args] = expr;
-    args = args.map(a => stringify(a));
-    if (fn in FN_STRINGIFY) return FN_STRINGIFY[fn](...args);
-    return fn + '(' + args.join(', ') + ')';
-  }
+  if (isNumber(expr) || isString(expr)) return expr;
+
+  let [fn, ...args] = expr;
+  args = args.map(a => stringify(a));
+  if (fn in FN_STRINGIFY) return FN_STRINGIFY[fn](...args);
+  return fn + '(' + args.join(', ') + ')';
 }
 
 
@@ -171,7 +256,8 @@ const FN_EVALUATE = {
   '!': n => factorial(n),
   '%': x => x / 100,
   '^': (a, b) => Math.pow(a, b),
-  'log': (x, b) => (b === undefined) ? Math.log(x) : Math.log(x) / Math.log(b)
+  'log': (x, b) => (b === undefined) ? Math.log(x) : Math.log(x) / Math.log(b),
+  '=': (a, b) => equals(a, b)
 };
 
 const CONSTANTS = {
@@ -180,92 +266,116 @@ const CONSTANTS = {
 };
 
 function evaluate(expr, vars={}) {
-  if (typeof expr === 'number') {
-    return expr;
+  if (isNumber(expr)) return expr;
 
-  } else if (typeof expr === 'string') {
-    if (expr in vars) return vars[expr];
+  if (isString(expr)) {
     let exprLower = expr.toLowerCase();
+    if (vars && exprLower in vars) return vars[expr];
     if (exprLower in CONSTANTS) return CONSTANTS[exprLower];
-    throw new Error(`Undefined variable "${expr}".`)
+    if (vars) throw new ExprError('EvalError', `The variable "${expr}" doesn’t exist.`);
+    return expr;
+  }
 
-  } else {
-    let [fn, ...args] = expr;
-    args = args.map(a => evaluate(a));
+  let [fn, ...args] = expr;
+  args = args.map(a => evaluate(a, vars));
+
+  if (vars || args.every(a => isNumber(a))) {
+    if (vars && fn in vars) return vars[fn](...args);
     if (fn in FN_EVALUATE) return FN_EVALUATE[fn](...args);
     if (fn in Math) return Math[fn](...args);
-    throw new Error(`Undefined function "${fn}".`)
   }
+
+  if (vars) throw new ExprError('EvalError', `The function "${fn}" doesn’t exist.`);
+  return [fn, ...args];
+}
+
+
+// -----------------------------------------------------------------------------
+// Extract functions and variables
+
+function variables(tree) {
+  if (isNumber(tree)) return [];
+  if (isString(tree)) return [tree];
+
+  let vars = [];
+  for (let a of tree.slice(1)) {
+    for (let v of variables(a)) {
+      if (!vars.includes(v)) vars.push(v);
+    }
+  }
+  return vars;
+}
+
+function functions(tree) {
+  if (isNumber(tree) || isString(tree)) return [];
+
+  let fns = [tree[0]];
+  for (let a of tree.slice(1)) {
+    for (let v of functions(a)) {
+      if (!fns.includes(v)) fns.push(v);
+    }
+  }
+  return fns;
 }
 
 
 // -----------------------------------------------------------------------------
 // Expression Parser
-// TODO handle implicit multiplication (consecutive expressions without +/-)
-// TODO handle functions with multiple arguments
+// TODO handle functions with multiple arguments (commas)
 
-const TOKEN_REGEX = /\b[0-9]+(.[0-9]*)?\b|\b\.[0-9]+\b|\b\w+\b|\+|-|–|±|\*|×|\/|÷|\^|%|!|\(|\)|\[|\]|\{|\}|\||,/g;
+const TOKEN_REGEX = /\b[0-9]+(\.[0-9]*)?\b|\b\.[0-9]+\b|\b\w+\b|\+|-|–|±|\*|×|\/|÷|\^|%|!|\(|\)|\[|\]|\{|\}|\||=|<|>|≤|≥/g;
+const TOKEN_REPLACE = {'–': '-', '÷': '/', '×': '*'};
 const BRACKETS = { '(': ')', '[': ']', '{': '}', '|': '|' };
 
-function parseMaths(tokens) {
+function findBinaryFunction(tokens, chars) {
+  for (let i=0; i<tokens.length; ++i) {
+    if (chars.includes(tokens[i])) {
+      let a = tokens[i-1];
+      let b = tokens[i+1];
+      if ('+-*/^%!'.includes(a)) throw new ExprError('SyntaxError', `A "${a}" cannot be followed by a "${tokens[i]}".`);
+      if ('+-*/^%!'.includes(b)) throw new ExprError('SyntaxError', `A "${tokens[i]}" cannot be followed by a "${b}".`);
+      tokens.splice(i - 1, 3, [tokens[i], a, b]);
+      i -= 2;
+    }
+  }
+}
 
-  // Handle Numbers
+function parseMaths(tokens) {
+  // Numbers
   for (let i=0; i<tokens.length; ++i) {
     let t = tokens[i];
     if (t ==  +t) tokens[i] = +t;
   }
 
-  // Handle Factorials and Percentages
+  if ('+*/^%!'.includes(tokens[0])) throw new ExprError('SyntaxError', `A term cannot start with a "${a}".`);
+
+  // Factorials and Percentages
   for (let i=0; i<tokens.length; ++i) {
-    if (tokens[i] == '!') {
-      tokens.splice(i-1, 2, ['!', tokens[i-1]]);
+    if ('!%'.includes(tokens[i])) {
+      tokens.splice(i-1, 2, [tokens[i], tokens[i-1]]);
       i -= 1;
-    } else if (tokens[i] == '%') {
-      tokens.splice(i-1, 2, ['%', tokens[i-1]]);
+    }
+  }
+
+  findBinaryFunction(tokens, '^');  // Powers
+  findBinaryFunction(tokens, '*/');  // Multiplication and division.
+
+  // Implicit multiplication (consecutive expressions)
+  for (let i=0; i<tokens.length-1; ++i) {
+    if (!'+*/^%!'.includes(tokens[i]) && !'+*/^%!'.includes(tokens[i+1])) {
+      tokens.splice(i, 2, ['*', tokens[i], tokens[i+1]]);
       i -= 1;
     }
   }
 
-  // Handle Powers
-  for (let i=0; i<tokens.length; ++i) {
-    if (tokens[i] == '^') {
-      tokens.splice(i-1, 3, ['^', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    }
-  }
+  // Leading (plus)minus.
+  if ('-±'.includes(tokens[0])) tokens.splice(0, 2, [tokens[0], tokens[1]]);
 
-  // Handle Multiplication and Division
-  for (let i=0; i<tokens.length; ++i) {
-    if ('/÷'.includes(tokens[i])) {
-      tokens.splice(i - 1, 3, ['/', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    } else if ('*×'.includes(tokens[i])) {
-      tokens.splice(i - 1, 3, ['*', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    }
-  }
+  findBinaryFunction(tokens, '+-±');  // Addition and subtraction.
+  findBinaryFunction(tokens, '=<>≤≥');  // Equalities and inequalities.
 
-  // TODO handle implicit multiplication (consecutive expressions without +/-)
-
-  // Handle leading minus.
-  if ('-–'.includes(tokens[0])) tokens.splice(0, 2, ['-', this.result[1]]);
-  if (tokens[0] == '±') tokens.splice(0, 2, ['±', this.result[1]]);
-
-  // Handle addition and subtraction.
-  for (let i=0; i<tokens.length; ++i) {
-    if ('-–'.includes(tokens[i])) {
-      tokens.splice(i-1, 3, ['-', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    } else if (tokens[i] === '+') {
-      tokens.splice(i-1, 3, ['+', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    } else if (tokens[i] === '±') {
-      tokens.splice(i-1, 3, ['±', tokens[i-1], tokens[i+1]]);
-      i -= 2;
-    }
-  }
-
-  return tokens;
+  if (tokens.length > 1) throw new ExprError('SyntaxError', `This expression is invalid.`);
+  return tokens[0];
 }
 
 function matchBrackets(tokens) {
@@ -273,24 +383,26 @@ function matchBrackets(tokens) {
 
   for (let i=0; i<tokens.length; ++i) {
     let t = tokens[i];
-    if (')]}|'.includes(t)) {
+    if (')]}'.includes(t) || (t == '|' && i > 0)) {
       return [parseMaths(newTokens), tokens.slice(i)]
 
     } else if ('([{|'.includes(t)) {
       let last = newTokens.pop();
-      if ('%!'.includes(last)) throw new Error(`A "${last}" cannot be followed by a "${t}".`);
+      if ('%!'.includes(last)) throw new ExprError('SyntaxError', `A "${last}" cannot be followed by a "${t}".`);
 
       let [inside, [close, ...rest]] = matchBrackets(tokens.slice(i + 1));
-      if (close != BRACKETS[t]) throw new Error(`Non-matching brackets "${t}" and "${close}".`);
+      if (close != BRACKETS[t]) throw new ExprError('SyntaxError', `The brackets "${t}" and "${close}" don’t match.`);
 
       // Vertical lines are automatically converted into abs.
       if (t == '|') inside = ['abs', inside];
 
       // Check last value of newTokens for possible function call.
-      if (last.match(/^\w+$/)) {
+      if (last && last.match(/^\w+$/) && last != +last) {
         newTokens.push([last, inside]);
-      } else {
+      } else if (last) {
         newTokens.push(last, inside);
+      } else {
+        newTokens.push(inside);  // This deals with expressions that start with "(".
       }
 
       // Update the for-loop parameters.
@@ -307,15 +419,16 @@ function matchBrackets(tokens) {
 
 function parseString(str) {
   let invalidChars = str.replace(TOKEN_REGEX, '').trim();
-  if (invalidChars) throw new Error(`Invalid character "${invalidChars[0]}".`);
+  if (invalidChars) throw new ExprError('SyntaxError', `The character "${invalidChars[0]}" is invalid.`);
 
   let tokens = str.match(TOKEN_REGEX);
+  for (let i=0; i<tokens.length; ++i) {
+    if (tokens[i] in TOKEN_REPLACE) tokens[i] = TOKEN_REPLACE[tokens[i]];
+  }
 
-  let [ast, rest] = matchBrackets(tokens);
-  if (rest.length) throw new Error(`Unexpected character "${rest[0][0]}".`);
-  if (ast.length  != 1) throw new Error(`Unknown parser error.`);
-
-  return ast[0];
+  let [expr, rest] = matchBrackets(tokens);
+  if (rest.length) throw new ExprError('SyntaxError', `The character "${rest[0][0]}" shouldn’t be here.`);
+  return expr;
 }
 
 
@@ -323,8 +436,13 @@ function parseString(str) {
 // Expressions Class
 
 export default class Expression {
-  constructor(str) { this.expr = simplify(parseString(str)); }
-  equals(other) { return equals(this.expr, other.expr); }
+  constructor(str, parse=true) { this.expr = parse ? parseString(str) : str; }
+
+  get simplified() { return new Expression(simplify(this.expr), false); }
+  get functions() { return functions(this.expr); }
+  get variables() { return variables(this.expr); }
+
+  equals(other) { return equals(this.simplified.expr, other.simplified.expr); }
   toString() { return stringify(this.expr); }
   evaluate(vars={}) { return evaluate(this.expr, vars); }
 }
--- tests/expression.js
+++ tests/expression.js
@@ -6,58 +6,59 @@
 
 import Expression from 'expression';
 
-String.prototype.contains = function(x) { return this.indexOf(x) >= 0 };
+function expr(src) { return new Expression(src).simplified; }
 
 
-function parse(src) {
-  let expr = new Expression(src);
-  return expr.toString();
-}
-
-
-exports['Simple expression parsing'] = function(test) {
-  test.equal(parse('1'), '1');
-  test.equal(parse('x + y'), 'x + y');
-  test.equal(parse('x + y + z'), 'x + y + z');
-  test.equal(parse('a*(b + c)'), 'a*(b + c)');
+exports['Parsing'] = function(test) {
+  test.equal(expr('1').toString(), '1');
+  test.equal(expr('-1').toString(), '-1');
+  test.equal(expr('x + y').toString(), 'x + y');
+  test.equal(expr('xx + yy + zz').toString(), 'xx + yy + zz');
+  test.equal(expr('a*(b + c)').toString(), 'a*b + a*c');
+  test.equal(expr('a + b*c^d').toString(), 'a + b*c^d');
+  test.equal(expr('a + (b*c)^d').toString(), 'a + b^d*c^d');
+  test.equal(expr('((a + b)*c)^d').toString(), 'a + b^d*c^d');
+  test.equal(expr('([{|a|}])').toString(), '|a|');
   test.done();
 };
 
-exports['Implicit Expression evaluation'] = function(test) {
-  test.equal(parse('1 + 2'), '3');
-  test.equal(parse('1 - 2'), '-1');
-  test.equal(parse('2*3'), '6');
-  test.equal(parse('5/2'), '2.5');
-  test.equal(parse('2^3'), '8');
-  test.equal(parse('sqrt(9)'), '3');
-  test.equal(parse('5%'), '0.05');
+exports['Advanced simplification and equality'] = function(test) {
+  /* compare('1/x + 1/y', '(x + y)/(x*y)');
+  compare('4 + 4*x - 2*(2 + 2*x))/(2 + 2*x)', '0');
+  compare('-4*x*y^2 - 2*y^3 - 2*x^2*y)/(x + y)^2', '-2*y');
+  compare('-x - y - (x + y)^(-1)*y^2 + (x + y)^(-1)*x^2', '-2*y');
+  compare('x + x*y)/x', '1 + y');
+  compare('f(x) + y*f(x))/f(x)', '1 + y');
+  compare('-x + y/(z + t) + z*x/(z + t) + z*a/(z + t) + t*x/(z + t)', '(y + a*z)/(z + t)');
+  compare('-x/-y)', 'x/y');
+  compare('-x/y)', '-x/y');
+  compare('x/y)', 'x/y');
+  compare('x/-y)', '-x/y');
+  compare('-x/0)', 'Infinity');
+  compare('-a*x/(-y - b))', 'a*x/(b + y)'); */
   test.done();
 };
 
+exports['Implicit evaluation'] = function(test) {
+  test.equal(expr('1 + 2').toString(), '3');
+  test.equal(expr('1 - 2').toString(), '-1');
+  test.equal(expr('2*3').toString(), '6');
+  test.equal(expr('5/2').toString(), '2.5');
+  test.equal(expr('2^3').toString(), '8');
+  test.equal(expr('sqrt(9)').toString(), '3');
+  test.equal(expr('5%').toString(), '0.05');
+  test.equal(expr('log(e)').toString(), '1');
+  test.done();
+};
 
+exports['Function and variable extraction'] = function(test) {
+  test.deepEqual(expr('a + b(c - d(5))').variables, ['a', 'c']);
+  test.deepEqual(expr('a + b(c - d(5))').functions, ['+', 'b', '-', 'd']);
+  test.done();
+};
 
-
-
-/* ;
- test.equal(parse('1/x + 1/y'), '(x + y)/(x*y)');
-compare('4 + 4*x - 2*(2 + 2*x))/(2 + 2*x)', '0');
-compare('-4*x*y^2 - 2*y^3 - 2*x^2*y)/(x + y)^2', '-2*y');
-compare('-x - y - (x + y)^(-1)*y^2 + (x + y)^(-1)*x^2', '-2*y');
-compare('x + x*y)/x', '1 + y');
-compare('f(x) + y*f(x))/f(x)', '1 + y');
-compare('-x + y/(z + t) + z*x/(z + t) + z*a/(z + t) + t*x/(z + t)', '(y + a*z)/(z + t)');
-compare('log(2) + log(3)', 'log(6)');
-compare('log(2*x) - log(2)', 'log(x)');
-
-compare('-x/-y)', 'x/y');
-compare('-x/y)', '-x/y');
-compare('x/y)', 'x/y');
-compare('x/-y)', '-x/y');
-compare('-x/0)', 'Infinity');
-compare('-a*x/(-y - b))', 'a*x/(b + y)');
-
-compare('0', '0');
-compare('-1', '-1');
-compare('1', '1');
-compare('1 + x', '1 + x');
-compare('2.7', '2.7'); */
+exports['Direct evaluation'] = function(test) {
+  test.equal(expr('a + b + sqrt(1)').evaluate({a: 1, b: 2}), 4);
+  test.equal(expr('a + b(a)').evaluate({a: 10, b: (x => x/2)}), 15);
+  test.done();
+};
