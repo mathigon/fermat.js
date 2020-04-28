b20cc2d289bd3f2d134c0d4ca738bb8b1504df78
Philipp Legner <philipp@mathigon.org>
2020-04-28 10:28:18 +0200

    Update build system to generate cjs and esm files

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-const typescript = require('rollup-plugin-typescript');
+import typescript from '@rollup/plugin-typescript';
 
-module.exports = {
+const options = {
   input: './index.ts',
-  plugins: [typescript(require('./tsconfig.json').compilerOptions)],
-  output: {
-    file: 'dist/fermat.js',
-    format: 'cjs',
-    name: 'app'
-  }
+  plugins: [typescript()],
+  external: ['@mathigon/core']
 };
+
+module.exports = [
+  {...options, output: {file: 'dist/fermat.cjs.js', format: 'cjs'}},
+  {...options, output: {file: 'dist/fermat.esm.js', format: 'esm'}}
+];
