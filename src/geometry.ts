02a0f1de853416b36101f4b982a9e0e1e661a7c7
Philipp Legner <philipp@mathigon.org>
2020-09-28 15:07:53 +0200

    New Ellipse class

--- src/geometry.ts
+++ src/geometry.ts
@@ -5,7 +5,7 @@
 
 
 import {total, flatten, toLinkedList, tabulate, isOneOf, last} from '@mathigon/core';
-import {nearlyEquals, isBetween, roundTo, square, clamp, lerp} from './arithmetic';
+import {nearlyEquals, isBetween, roundTo, square, clamp, lerp, quadratic} from './arithmetic';
 import {subsets} from './combinatorics';
 
 
@@ -668,6 +668,67 @@ export class Arc {
 
 export class Sector extends Arc {
   readonly type = 'sector';
+}
+
+export class Ellipse {
+  readonly type = 'ellipse';
+  f1: Point;
+  f2: Point;
+
+  constructor(readonly c: Point, readonly a: number, readonly b: number) {
+    if (a < b) throw new Error('Vertical ellipses not supported.');
+    const f = Math.sqrt(a ** 2 - b ** 2);  // Distance from focus to the center.
+    this.f1 = c.shift(-f, 0);
+    this.f2 = c.shift(f, 0);
+  }
+
+  at(t: number) {
+    const th = 2 * Math.PI * t;
+    return this.c.shift(this.a * Math.cos(th), this.b * Math.sin(th));
+  }
+
+  // TODO Support vertical and rotated ellipses
+  // TODO Add rotate, reflect, scale, shift, translate, contains, equals methods
+
+  normalAt(p: Point) {
+    const a = new Angle(this.f1, p, this.f2);
+    return a.bisector!;
+  }
+
+  /** Project a point onto an ellipse. */
+  project(p: Point) {
+    const [a, b] = [this.a, this.b];
+    const th = p.angle(this.c);
+    const k = a * b / Math.sqrt((b * Math.cos(th)) ** 2 + (a * Math.sin(th)) ** 2);
+    return new Point(k * Math.cos(th), k * Math.sin(th));
+  }
+
+  /** Intersection between an ellipse and a line. */
+  intersect(line: Line) {
+    const dx = line.p1.x - line.p2.x;
+    const dy = line.p1.y - line.p2.y;
+
+    const px = this.c.x - line.p1.x;
+    const py = this.c.y - line.p1.y;
+
+    const A = (dx / this.a) ** 2 + (dy / this.b) ** 2;
+    const B = 2 * px * dx / (this.a) ** 2 + 2 * py * dy / (this.b) ** 2;
+    const C = (px / this.a) ** 2 + (py / this.b) ** 2 - 1;
+
+    const points = quadratic(A, B, C);
+    return points.map(t => line.at(t));
+  }
+
+  /**
+   * Creates a new Ellipse. StringLength is the length of string from one foci
+   * to a point on the circumference, to the other foci.
+   */
+  static fromFoci(f1: Point, f2: Point, stringLength: number) {
+    const c = Point.distance(f1, f2) / 2;  // Half distance between foci.
+    const a = stringLength / 2;
+    const b = Math.sqrt(a ** 2 - c ** 2);
+    return new Ellipse(Point.interpolate(f1, f2), a, b);
+  }
 }
 
 
